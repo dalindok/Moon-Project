bd2865ab77a828a7d17dd7cbbc81f6dde15df684
Thonida1045 <ithonida@gmail.com>
2024-11-01 10:28:08 +0700

    coreValue

--- src/components/CoreValue.tsx
+++ src/components/CoreValue.tsx
@@ -7,22 +7,21 @@ function CoreValue() {
                 Our Core Values
             </p>
             <div className='flex flex-col items-center mt-10'>
-                <div className='flex flex-row justify-between w-full max-w-4xl'>
-                    <div className='bg-pink-200 ml-10 mr-4 rounded-lg flex-grow h-full p-4'>
+                <div className='flex flex-row justify-between'>
+                    <div className='bg-pink-200 ml-10 mr-4 rounded-lg flex-grow w-96 h-auto p-4'>
                         <p className='font-bold'>Community Support:</p>
-                        <p>Connect, share, and grow with</p>
-                        <p>a supportive skincare community.</p>
+                        <p>Connect, share, and grow with a supportive skincare community.</p>
                     </div>
-                    <div className='bg-pink-200 ml-4 mr-10 rounded-lg flex-grow h-full p-4'>
+                    <div className='bg-pink-200 ml-4 mr-10 rounded-lg flex-grow w-96 h-auto p-4'>
                         <p className='font-bold'>Quality & Wellness:</p>
-                        <p>Embrace a path to radiant skin</p>
-                        <p>with products and routines that prioritize your well-being.</p>
+                        <p>Embrace a path to radiant skin with products and routines that prioritize your well-being.</p>
+                        <p></p>
                     </div>
                 </div>
-                <div className='bg-pink-200 rounded-lg max-w-4xl h-auto justify-center mt-10 p-4 flex-grow'>
+                <div className='bg-pink-200 rounded-lg w-96 h-auto justify-center mt-10 p-4 flex-grow'>
                     <p className='font-bold'>Expert Insights:</p>
-                    <p>Discover trusted tips and advice to</p>
-                    <p>enhance your skincare journey.</p>
+                    <p>Discover trusted tips and advice to enhance your skincare journey.</p>
+                    <p></p>
                 </div>
             </div>
         </div>
