88fb7335845e54a6c286311743d6c34f5544afea
Thonida1045 <ithonida@gmail.com>
2024-11-08 14:45:08 +0700

    Update Goole logo

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,33 +1,35 @@
 import React from "react";
-import { FaGoogle, FaFacebook } from 'react-icons/fa';
+import {FaFacebook } from 'react-icons/fa';
+import { ReactComponent as GoogleIcon } from '../assets/GoogleIcon.svg';
+
 function Login() {
     return (
       <div>
-        <div className="bg-pink-100 rounded-md p-8 w-96 mr-auto ml-auto">
-          <div className="bg-white rounded-md p-8">
+        <div className="bg-customPink rounded-md p-8 w-96 mr-auto ml-auto font-zain">
+          <div className="bg-white rounded-md p-8"> 
             <div className="my-3">
-            <p className="text-black font-bold text-center text-3xl">Login</p>
+            <p className="text-black font-bold text-center text-6xl">Login</p>
             </div>
-            <form className="flex flex-col gap-5 my-9">
+            <form className="flex flex-col gap-2 my-9">
                 <p className="text-start font-semibold">Username</p>
               <input
                 type="Text"
                 placeholder="Type your username"
-                className="rounded-xl px-3 py-3 placeholder:text-base bg-slate-100 text-gray-700" 
+                className="rounded-xl px-3 py-3 placeholder:text-base bg-gray text-gray-700" 
               />
              <p className="text-start font-semibold">Password</p>
               <input
                 type="password"
                 placeholder="type your password"
-                className="rounded-xl px-3 py-3 placeholder:text-base bg-slate-100 text-gray-700" 
+                className="rounded-xl px-3 py-3 placeholder:text-base bg-gray text-gray-700" 
               />
             </form>
-            <button className="bg-pink-100 text-black text-xl font-bold px-24 py-2 rounded-md">
+            <button className="bg-customPink text-black text-xl font-bold px-24 py-2 rounded-md">
               LOGIN
             </button>
           <p className="text-black font-thin mt-3 ">Or Sign Up with </p>
-          <div>
-      <FaGoogle size={24} color="#DB4437" /> {/* Google Icon in red */}
+          <div className="flex flex-row justify-center items-center space-x-4  mt-5">
+          <GoogleIcon width="24px" height="24px" />
       <FaFacebook size={24} color="#1877F2" /> {/* Facebook Icon in blue */}
     </div>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      customPink: '#f4c5c2',
+      white: '#ffffff',
+      gray: '#f1f5f9',
+    },
+    fontFamily: {
+      zain: ['Zain', 'sans-serif'], // Add fallback font family if needed
+    },
   },
   plugins: [],
 }
